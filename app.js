786eec4dd20cd63a9c54df02adfb191c4067c40a
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-23 13:30:50 +0300

    Отрисовка полученных задач

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 // Globals
+const todoList = document.getElementById("todo-list");
 let todos = [];
 let users = [];
 // Attach Events
 document.addEventListener("DOMContentLoaded", initApp);
+
+// Basic Logic
+function getUserName(userId){
+    const user = users.find(u => u.id === userId)
+    return user.name;
+}
+
+function printTodo({id, userId, title, completed}){
+    const li = document.createElement("li");
+    li.className = "todo-item";
+    li.dataset.id = id;
+    li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(userId)}</b></span>`;
+
+    const status = document.createElement("input");
+    status.type = "checkbox";
+    status.checked = completed;
+    
+    const close = document.createElement("span");
+    close.innerHTML = '&times;';
+    close.className = "close";
+
+    li.prepend(status);
+    li.append(close);
+    
+    todoList.prepend(li);
+}
 
 // Event Logic
 function initApp(){
@@ -10,7 +37,9 @@ function initApp(){
        [todos, users] = values;
         console.log(todos);
         console.log(users);
+
        // Отправить в разметку
+       todos.forEach(todo => printTodo(todo))
     })
 }
 
