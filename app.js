20ea82569ce297fc33ccd345eda559b0222f76ab
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-23 19:35:21 +0300

    Изменение статуса по чекбоксу

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ function printTodo({id, userId, title, completed}){
     const status = document.createElement("input");
     status.type = "checkbox";
     status.checked = completed;
+    status.addEventListener("change", handleTodoChange)
     
     const close = document.createElement("span");
     close.innerHTML = '&times;';
@@ -68,6 +69,13 @@ function handleSubmit(event){
     })
 }
 
+function handleTodoChange(){
+    const todoId = this.parentElement.dataset.id;
+    const completed = this.checked;
+    console.log(todoId, completed);
+    toggleTodoComplete(todoId, completed);
+}
+
 // Async logic
 async function getAllTodos(){
     const response = await fetch("https://jsonplaceholder.typicode.com/todos")
@@ -96,4 +104,20 @@ async function createTodo(todo){
     console.log(newTodo);
 
     printTodo(newTodo);
+}
+
+async function toggleTodoComplete(todoId, completed){
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
+        method: "PATCH",
+        body: JSON.stringify({completed}),// completed: completed
+        headers: {
+            'Content-Type': 'application/json',// в качестве ответа ожидаем json
+        },
+    })
+    const data = await response.json();
+    console.log(data);
+
+    if(!response.ok) {
+        // Error
+    }
 }
